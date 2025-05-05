f8843cd22bbdba86de75dbdb1c7df0b0135a3cf7
Amit Shalvi Kali <a.shalvikali@gmail.com>
2025-05-05 16:36:58 +0300

    fixed main

--- main.cpp
+++ main.cpp
@@ -7,19 +7,19 @@
 int main()
 {
     // building from the bottom up
-    std::unique_ptr<TreeNode> N3 = std::make_unique<TreeNode>(std::string("N3"));
-    N3->add_child(std::make_unique<TreeNode>(std::string("Dana")), 6);
+    std::shared_ptr<TreeNode> N3 = TreeNode::make(std::string("N3"));
+    N3->add_child(TreeNode::make(std::string("Dana")), 6);
 
-    std::unique_ptr<TreeNode> N1 = std::make_unique<TreeNode>(std::string("N1"));
-    N1->add_child(std::make_unique<TreeNode>(std::string("Tal")), 2);
-    N1->add_child(std::make_unique<TreeNode>(std::string("Noam")), 3);
+    std::shared_ptr<TreeNode> N1 = TreeNode::make(std::string("N1"));
+    N1->add_child(TreeNode::make(std::string("Tal")), 2);
+    N1->add_child(TreeNode::make(std::string("Noam")), 3);
 
-    std::unique_ptr<TreeNode> N2 = std::make_unique<TreeNode>(std::string("N2"));
-    N2->add_child(std::make_unique<TreeNode>(std::string("Ben")), 4);
-    N2->add_child(std::move(N1), 1);
-    N3->add_child(std::move(N2), 1.5);
+    std::shared_ptr<TreeNode> N2 = TreeNode::make(std::string("N2"), N3, 1.5); // setting parent, but parent will not know child
+    N2->add_child(TreeNode::make(std::string("Ben")), 4);
+    N2->add_child(N1, 1);
+    N3->add_child(N2, 1.5);
 
-    // Example - make with children
-    // std::unique_ptr<TreeNode> Ex(TreeNode::make_with_children("N1", {std::move(Tal), std::move(Noam)}, std::move(Ben), 1).get());
+    // Example - make with children. does not support giving distances.
+    std::shared_ptr<TreeNode> N4 = TreeNode::make_with_children(std::string("N4"), {N3});
     return 0;
 }
